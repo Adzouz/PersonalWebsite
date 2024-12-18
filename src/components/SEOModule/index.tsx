9b15c44b723e1f0dc9571bc08024c976e01cee81
Adrien Lepoutre <3693028+Adzouz@users.noreply.github.com>
2024-12-18 16:04:32 -0500

    fix: move static metas to index.html fix: remove useless service worker and manifest

--- src/components/SEOModule/index.tsx
+++ src/components/SEOModule/index.tsx
@@ -13,20 +13,11 @@ const SEOModule = ({
   <Helmet>
     <html lang={lang} />
     <title>{title}</title>
-    <meta name="robots" content="index, follow" />
-    <meta content="Adrien Lepoutre" name="author" />
-    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="description" content={description} />
     <meta name="keywords" content={keywords} />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
     <meta property="og:url" content={url} />
-    <meta property="og:type" content="website" />
-    <meta
-      property="og:image"
-      content="https://thewebsdoor.com/images/facebook.webp"
-    />
-    <meta content="386568964690808" property="fb:app_id" />
     <link rel="canonical" href={url} />
     {alternateUrls.map(({ lang, url }) => (
       <link key={lang} rel="alternate" hrefLang={lang} href={url} />
